d476009b6a0c131f43413f7a8d1de3cc7ddd7dde
Supanat Litchaweerat <6331305038@lamduan.mfu.ac.th>
2022-11-28 02:12:41 +0700

    commit

--- project-enterprise/pages/index.vue
+++ project-enterprise/pages/index.vue
@@ -1,7 +1,127 @@
 <template>
-  <div>
-    <h2>
-      kuy
-    </h2>
-  </div>
+  <v-row>
+    <div>
+      <v-col></v-col>
+      <h2>Recomend course</h2>
+      <v-col style="height: 10px"></v-col>
+      <v-row>
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn href="/detail" color="white" text> Get </v-btn>
+        </v-card>
+
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn color="white" text> Get </v-btn>
+        </v-card>
+
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn color="white" text> Get </v-btn>
+        </v-card>
+
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn color="white" text> Get </v-btn>
+        </v-card>
+      </v-row>
+
+      <v-col></v-col>
+      <h2>Unity</h2>
+      <v-col style="height: 10px"></v-col>
+      <v-row>
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn color="white" text> Get </v-btn>
+        </v-card>
+
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn color="white" text> Get </v-btn>
+        </v-card>
+
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn color="white" text> Get </v-btn>
+        </v-card>
+
+        <v-card class="mr-9 mx-auto" max-width="400">
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            height="200px"
+          ></v-img>
+
+          <v-card-title> Top western road trips </v-card-title>
+
+          <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+          <v-spancer></v-spancer>
+          <v-btn color="white" text> Get </v-btn>
+        </v-card>
+      </v-row>
+    </div>
+  </v-row>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+};
+</script>
