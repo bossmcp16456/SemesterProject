3a4d7fdbef6c208d4f61b159e2de33dcd2ae177b
PRASOPSUK KHENDA <6331305016@lamduan.mfu.ac.th>
2022-11-29 04:08:58 +0700

    game commit

--- project-enterprise/src/main/java/com/mfu/projectenterprise/model/BookCourse.java
+++ project-enterprise/src/main/java/com/mfu/projectenterprise/model/BookCourse.java
@@ -1,5 +1,76 @@
 package com.mfu.projectenterprise.model;
+import java.sql.Date;
+import java.util.Set;
 
+import javax.xml.crypto.Data;
+
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 public class BookCourse {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="COURSE_ID")
+    private int id;
+
+    @Column(name="NAME")
+    private int courseid;
+
+    @Column(name="TEACHER_ID")
+    private int teacherid;
+
+    @Column(name="PRICE")
+    private Date datetime;
+
+    // @JsonIgnore
+    // @OneToMany(mappedBy = "user")
+    // private Set<Booking> booking;
+
+    public BookCourse() {}
+
+    public BookCourse(int id, int courseid,int teacherid, Date datetime) {
+        this.id = id;
+        this.courseid = courseid;
+        this.teacherid = teacherid;
+        this.datetime = datetime;
+    }
+
+    public BookCourse(int courseid,int teacherid, Date datetime) {
+        this((Integer) null, courseid,teacherid, datetime);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCourseId() {
+        return courseid;
+    }
+
+    public int getTeacherId() {
+        return teacherid;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCourseId(int courseid) {
+        this.courseid = courseid;
+    }
+
+    public void setTeacherId(int teacherid) {
+        this.teacherid = teacherid;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+   
 }
--- project-enterprise/src/main/java/com/mfu/projectenterprise/model/Course.java
+++ project-enterprise/src/main/java/com/mfu/projectenterprise/model/Course.java
@@ -1,5 +1,74 @@
 package com.mfu.projectenterprise.model;
-
+import java.util.Set;
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 public class Course {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="COURSE_ID")
+    private int id;
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="TEACHER_ID")
+    private int teacherid;
+
+    @Column(name="PRICE")
+    private int price;
+
+    // @JsonIgnore
+    // @OneToMany(mappedBy = "user")
+    // private Set<Booking> booking;
+
+    public Course() {}
+
+    public Course(int id, String name,int teacherid, int price) {
+        this.id = id;
+        this.name = name;
+        this.teacherid = teacherid;
+        this.price = price;
+    }
+
+    public Course(String name,int teacherid, int price) {
+        this((Integer) null, name,teacherid, price);
+    }
+
+   
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTeacherId() {
+        return teacherid;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeacherId(int teacherid) {
+        this.teacherid = teacherid;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+   
 }
